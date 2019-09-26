fd8acc0ad5c98beb873027858c76eecf3b317b79
romanhaa <roman.hillje@googlemail.com>
2019-09-26 15:25:29 +0200

    Remove comments from trajectory server.R file and rename ui.R to UI.R.

--- inst/shiny/trajectory/server.R
+++ inst/shiny/trajectory/server.R
@@ -3,14 +3,7 @@
 ##----------------------------------------------------------------------------##
 
 # what needs to be done
-# - check if trajectory data is available
-# - if not, display a message
-# - if yes, merge with rest of meta data (so cells can be colored by whatever)
-#   - maybe just merge with those cells that info is available for? not ideal
-# - plot trajectory with ggplot -> plotly
-# - second plot to show selected meta data over pseudotime
 # - let user show gene expression as well? probably more complicated
-
 
 # UI element: display results or alternative text
 output[["trajectory_UI"]] <- renderUI({
