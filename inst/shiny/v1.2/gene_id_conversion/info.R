0f249085de408b01dacf4989f761fbcf4dd1f53e
Roman Hillje <roman.hillje@googlemail.com>
2020-03-07 15:44:34 +0100

    Just do it.

--- inst/shiny/v1.2/gene_id_conversion/info.R
+++
@@ -1,8 +0,0 @@
-##----------------------------------------------------------------------------##
-## Tab: Gene id/symbol conversion.
-##----------------------------------------------------------------------------##
-
-geneIdConversion_info <- list(
-  title = "Gene ID/symbol conversion",
-  text = p("Conversion table containing Gencode identifiers, Ensembl identifiers, Havana identifiers, gene symbol and gene type for mouse (version M16) and human (version 27).")
-)
