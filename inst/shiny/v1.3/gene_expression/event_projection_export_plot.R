8a792cc0caf60d9e1351984a570c5f14bb18ec15
Roman Hillje <roman.hillje@googlemail.com>
2021-03-03 21:35:51 +0100

    Show modal when trying to export projection with multiple panel

--- inst/shiny/v1.3/gene_expression/event_projection_export_plot.R
+++ inst/shiny/v1.3/gene_expression/event_projection_export_plot.R
@@ -6,9 +6,10 @@ observeEvent(input[["expression_projection_export"]], {
   ## assign input to variables
   input_data <- expression_projection_data_to_plot()
   cells_df <- input_data[['cells_df']]
+  coordinates <- input_data[['coordinates']]
   expression_levels <- input_data[['expression_levels']]
   cells_df$level <- expression_levels
-  cells_df <- bind_cols(input_data[['coordinates']], cells_df)
+  cells_df <- bind_cols(coordinates, cells_df)
   plot_parameters <- input_data[['plot_parameters']]
   color_settings <- input_data[['color_settings']]
   trajectory <- input_data[['trajectory']]
@@ -40,21 +41,30 @@ observeEvent(input[["expression_projection_export"]], {
     cell_order <- order(expression_levels)
     cells_df <- cells_df[cell_order,]
   }
-  ## check if projection or trajectory should be shown
-  ## ... projection
-  if ( plot_parameters[["projection"]] %in% availableProjections() ) {
-    ## check if selection projection consists of 2 or 3 dimensions
-    ## ... selection projection consists of 3 dimensions
-    if ( ncol(getProjection(plot_parameters[["projection"]])) == 3 ) {
-      ## give error message
-      shinyWidgets::sendSweetAlert(
-        session = session,
-        title = "Sorry!",
-        text = "It's currently not possible to create PDF plots from 3D dimensional reductions. Please use the PNG export button in the panel or a 2D dimensional reduction instead.",
-        type = "error"
-      )
-    ## ... selection projection consists of 2 dimensions
-    } else if ( ncol(getProjection(plot_parameters[["projection"]])) == 2 ) {
+  ##
+  if (
+    is.list(expression_levels) ||
+    ncol(coordinates) == 3
+  ) {
+    shinyWidgets::sendSweetAlert(
+      session = session,
+      title = "Sorry!",
+      text = HTML("
+        The plot could not be exported to a PDF file.<br>
+        Possible reasons:
+        <ul>
+          <li>You have selected a 3D dimensional reduction.</li>
+          <li>Expression is shown in a separate panel per gene.</li>
+        </ul>
+        Please use the PNG export button in the top-right corner of the projection instead."
+      ),
+      type = "error",
+      html = TRUE
+    )
+  } else {
+    ## check if projection or trajectory should be shown
+    ## ... projection
+    if ( plot_parameters[["projection"]] %in% availableProjections() ) {
       ## ... separate panels requested and "gene" column present
       if (
         input[["expression_projection_genes_in_separate_panels"]] == TRUE &&
@@ -84,45 +94,45 @@ observeEvent(input[["expression_projection_export"]], {
           y_range = plot_parameters[["y_range"]]
         )
       }
+    ## ... trajectory
+    } else {
+      ## prepare plot
+      plot <- pltExpTrj2DSglPanExp(
+        df = cells_df,
+        trajectory_edges = trajectory[["edges"]],
+        point_size = plot_parameters[["point_size"]],
+        point_opacity = plot_parameters[["point_opacity"]],
+        point_border = plot_parameters[["draw_border"]],
+        color_scale = color_settings[["color_scale"]],
+        color_range = color_settings[["color_range"]],
+        x_range = plot_parameters[["x_range"]],
+        y_range = plot_parameters[["y_range"]]
+      )
     }
-  ## ... trajectory
-  } else {
-    ## prepare plot
-    plot <- pltExpTrj2DSglPanExp(
-      df = cells_df,
-      trajectory_edges = trajectory[["edges"]],
-      point_size = plot_parameters[["point_size"]],
-      point_opacity = plot_parameters[["point_opacity"]],
-      point_border = plot_parameters[["draw_border"]],
-      color_scale = color_settings[["color_scale"]],
-      color_range = color_settings[["color_range"]],
-      x_range = plot_parameters[["x_range"]],
-      y_range = plot_parameters[["y_range"]]
-    )
-  }
-  ## plot must be a ggplot object, otherwise don't proceed
-  req(is.ggplot(plot))
-  ## save plot
-  pdf(NULL)
-  ggsave(save_file_path, plot, height = 8, width = 11)
-  ## check if file was succesfully saved
-  ## ... successful
-  if ( file.exists(save_file_path) ) {
-    ## give positive message
-    shinyWidgets::sendSweetAlert(
-      session = session,
-      title = "Success!",
-      text = paste0("Plot saved successfully as: ", save_file_path),
-      type = "success"
-    )
-  ## ... failed
-  } else {
-    ## give negative message
-    shinyWidgets::sendSweetAlert(
-      session = session,
-      title = "Error!",
-      text = "Sorry, it seems something went wrong...",
-      type = "error"
-    )
+    ## plot must be a ggplot object, otherwise don't proceed
+    req(is.ggplot(plot))
+    ## save plot
+    pdf(NULL)
+    ggsave(save_file_path, plot, height = 8, width = 11)
+    ## check if file was succesfully saved
+    ## ... successful
+    if ( file.exists(save_file_path) ) {
+      ## give positive message
+      shinyWidgets::sendSweetAlert(
+        session = session,
+        title = "Success!",
+        text = paste0("Plot saved successfully as: ", save_file_path),
+        type = "success"
+      )
+    ## ... failed
+    } else {
+      ## give negative message
+      shinyWidgets::sendSweetAlert(
+        session = session,
+        title = "Error!",
+        text = "Sorry, it seems something went wrong...",
+        type = "error"
+      )
+    }
   }
 })
