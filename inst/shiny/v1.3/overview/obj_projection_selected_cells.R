6e624edf041b6628fb37ed67065cc69de4f0cf94
Roman Hillje <roman.hillje@googlemail.com>
2021-03-06 14:55:43 +0100

    Remove projection coordinates from the dataframe of selected cells
    Applies to projection in overview tab. Also, add debugging messages to
    reactive object holding selected cells.

--- inst/shiny/v1.3/overview/obj_projection_selected_cells.R
+++ inst/shiny/v1.3/overview/obj_projection_selected_cells.R
@@ -5,9 +5,10 @@
 overview_projection_selected_cells <- reactive({
   ## make sure plot parameters are set because it means that the plot can be
   ## generated
-  req(
-    overview_projection_data_to_plot()
-  )
+  req(overview_projection_data_to_plot())
+  if (exists('mode_debugging') && grepl('v', mode_debugging, ignore.case=TRUE)) {
+    message('--> trigger "overview_projection_selected_cells"')
+  }
   if (!is.null(input[['overview_projection_brush']])) {
     req(
       input[['overview_projection_brush']][['mapping']][['x']] %in%
@@ -16,10 +17,14 @@ overview_projection_selected_cells <- reactive({
   }
   selected_cells <- brushedPoints(
       cbind(
-        overview_projection_data_to_plot()[['cells_df']],
-        overview_projection_data_to_plot()[['coordinates']]
+        overview_projection_data_to_plot()[['coordinates']],
+        overview_projection_data_to_plot()[['cells_df']]
       ),
       input[['overview_projection_brush']]
-    )
+    ) %>%
+    dplyr::select(3:ncol(.))
+  if (exists('mode_debugging') && grepl('vv', mode_debugging, ignore.case=TRUE)) {
+    message(str(selected_cells))
+  }
   return(selected_cells)
 })
