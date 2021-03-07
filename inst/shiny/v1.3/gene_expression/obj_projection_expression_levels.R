5c9ffda3584a95d683656ea8fa09da6a1580e52f
Roman Hillje <roman.hillje@googlemail.com>
2021-03-07 10:38:04 +0100

    Fetch expression data from API

--- inst/shiny/v1.3/gene_expression/obj_projection_expression_levels.R
+++ inst/shiny/v1.3/gene_expression/obj_projection_expression_levels.R
@@ -3,12 +3,12 @@
 ##----------------------------------------------------------------------------##
 expression_projection_expression_levels <- reactive({
   req(
-    expression_projection_data(),
+    expression_projection_cells_to_show(),
     expression_selected_genes()
   )
   # message('--> trigger "expression_projection_expression_levels"')
   if ( length(expression_selected_genes()$genes_to_display_present) == 0 ) {
-    expression_levels <- rep(0, nrow(expression_projection_data()))
+    expression_levels <- rep(0, length(expression_projection_cells_to_show()))
   } else {
     req(expression_projection_coordinates())
     if (
@@ -27,10 +27,14 @@ expression_projection_expression_levels <- reactive({
         expression_levels[[colnames(expression_matrix)[i]]] <- as.vector(expression_matrix[,i])
       }
     } else {
-      expression_levels <- unname(getMeanExpressionForCells(
-        cells = expression_projection_data()$cell_barcode,
-        genes = expression_selected_genes()$genes_to_display_present
-      ))
+      genes <- paste0(expression_selected_genes()$genes_to_display_present, collapse=',')
+      message(str(genes))
+      raw_result <- httr::GET(url = 'http://localhost:8000', path = glue::glue('test?gene={genes}'))
+      raw_content <- rawToChar(raw_result$content)
+      expression_levels <- jsonlite::fromJSON(raw_content)
+      message(str(expression_levels))
+      expression_levels <- expression_levels[expression_projection_cells_to_show()]
+      message(str(expression_levels))
     }
   }
   # message(str(expression_levels))
